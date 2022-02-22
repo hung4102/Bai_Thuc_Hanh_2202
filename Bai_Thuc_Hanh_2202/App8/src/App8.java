2ac9d58ecb3b09fe2d8b34e4d18b6ff08c29a002
hung4102 <97976728+hung4102@users.noreply.github.com>
2022-02-22 15:50:24 +0700

    Update App8.java

--- Bai_Thuc_Hanh_2202/App8/src/App8.java
+++ Bai_Thuc_Hanh_2202/App8/src/App8.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
-import static java.lang.System.*;
 
 public class App8 {
     public static void main(String[] args) {
-        var sc=new Scanner(in);
-        int max=0;
-        int x = 1;
-        while ( x !=0){
-            out.println("Nhap so thuc :");
-            x = sc.nextInt();
-            if(max%5==0) max=x;
-        }
-        out.println("Max= "+max);
-    }
-    }
+        float n;
+        float max = 0;
 
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Nhap vao 1 so thuc: ");
+            n = sc.nextFloat();
+            if (n > max && n % 5 == 0){
+                max = n;
+            }
+
+        } while (n != 0);
+        System.out.println("Gia tri lon nhat trong cac so chia het cho 5 vua nhap: " + max);
+
+    }
+}
 
