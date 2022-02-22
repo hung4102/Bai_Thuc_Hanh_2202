676a31e88974b26ab6e244973a14ed3ccbc60d06
hung4102 <97976728+hung4102@users.noreply.github.com>
2022-02-22 15:29:32 +0700

    Add files via upload

--- Bai_Thuc_Hanh_2202/App10/src/App10.java
+++ Bai_Thuc_Hanh_2202/App10/src/App10.java
@@ -3,14 +3,24 @@ import static java.lang.System.*;
 
 public class App10 {
     public static void main(String[] args) {
-        var sc= new Scanner(in);
-        int n=sc.nextInt();
-        int dem=0;
-        while(n>=10){
-            n/=10; // hay n = n /10;
-            dem++;
-        }
-        out.println("Số " +n+ " có "++" chữ số" );
-    }
+        var sc = new Scanner(in);
 
+        long temp;
+        int count = 0;
+        int n;
+        do {
+            out.println("Nhập vào số nguyên n lớn hơn 0: ");
+            n = sc.nextInt();
+            if (n <= 0) {
+                out.println(" Số n phải lớn hơn 0, vui lòng nhập lại !");
+            }
+        } while (n <= 0);
+        temp = n;
+        if (n == 0) count = 1;
+        while (temp != 0) {
+            count++;
+            temp = temp / 10;
+        }
+        out.println("Số chữ số là "+count);
+    }
 }
